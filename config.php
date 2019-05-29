61316338fa90a5f037226633e7f967b4f5d3a4b9
Hack Solustion <50937775+hacksolustion@users.noreply.github.com>
2019-05-29 08:31:39 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+//delay min 1 sec max 9999999 sec :v
+$delay=10;
+//$config = ["token",];
+$config = [
+	   "95ff4118-1358-4016-bdc8-2e5121d66d3c",
+	   "5e992312-1b4f-4f55-bb04-e9967b9ce9f8",
+	   "afbfd928-4892-4178-a2e9-c96bb78c8691",
+	   "3efef9b6-a7c9-44f1-bf8f-58a0ea2b58ea",
+
+];
+
